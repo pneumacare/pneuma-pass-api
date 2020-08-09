d74cbad9a1670cda82b84cbeeba629520243f51d
Samuel Okoroafor <samuel.i.okoroafor@gmail.com>
2020-08-09 22:44:35 +0200

    deployment changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,10 @@
 import { initializeApp, credential, ServiceAccount } from "firebase-admin";
-import serviceAccount from "../google-service-account.json";
+import fs from "fs"
+
+const content = fs.readFileSync(__dirname + "/google-service-account.json")
+
+const serviceAccount = JSON.parse(content.toString());
+
 const firebaseApp = initializeApp({
     credential: credential.cert(serviceAccount as ServiceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL
